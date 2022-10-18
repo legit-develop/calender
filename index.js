bb3c200ea1b1300de588953c73bfd80304562a9b
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2022-10-18 11:37:43 +0200

    add month and days dynamically

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ console.log(monthEl);
 const fullDateEl = document.querySelector(".date p");
 console.log(fullDateEl);
 
+const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
+console.log(lastDay);
+
+const daysEl = document.querySelector(".days");
+console.log(daysEl);
+
+const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1;
+console.log(firstDay);
+
 //get array of month
 const months = [
   "January",
@@ -26,3 +35,19 @@ console.log(months[monthInx]);
 
 monthEl.innerText = months[monthInx];
 fullDateEl.innerText = new Date().toDateString();
+
+let days = "";
+
+for (let i = firstDay; i > 0; i--) {
+  days += `<div class="empty"></div>`;
+}
+
+for (let i = 1; i <= lastDay; i++) {
+  if (i === new Date().getDate()) {
+    days += `<div class='today'>${i}</div>`;
+  } else {
+    days += `<div>${i}</div>`;
+  }
+}
+
+daysEl.innerHTML = days;
