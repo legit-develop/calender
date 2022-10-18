dc26b627dd9de1c995587012a942abd696614f9e
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2022-10-18 10:39:20 +0200

    create a varibale for date and dynamicall add it

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const monthInx = new Date().getMonth();
 console.log(monthInx); //will print the index of the month
 const monthEl = document.querySelector(".date h1");
 console.log(monthEl);
+const fullDateEl = document.querySelector(".date p");
+console.log(fullDateEl);
 
 //get array of month
 const months = [
@@ -23,3 +25,4 @@ const months = [
 console.log(months[monthInx]);
 
 monthEl.innerText = months[monthInx];
+fullDateEl.innerText = new Date().toDateString();
